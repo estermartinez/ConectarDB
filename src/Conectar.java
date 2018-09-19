2a2a8a3362a0c1ab47b6d0c27e164fbe76e379f9
estermartinez <43380439+estermartinez@users.noreply.github.com>
2018-09-19 10:56:19 +0200

    primer commit

---
+++ src/Conectar.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conectar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost", "root", "caracoles");
+			System.out.println("Conexion exitosa");
+		}catch(Exception e){
+			System.out.println(e);
+			
+		}
+	}
+
+}
